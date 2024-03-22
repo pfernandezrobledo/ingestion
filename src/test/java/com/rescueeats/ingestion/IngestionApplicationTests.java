94f3888f2bab176176d5adb478bafc66e1396f76
Pedro Fernandez <ingpedro@gmail.com>
2024-03-21 17:30:13 -0700

    initial service for ingestion

---
+++ src/test/java/com/rescueeats/ingestion/IngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rescueeats.ingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
